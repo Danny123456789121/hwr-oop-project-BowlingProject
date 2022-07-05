2e323f18f130f831cb1326e28fbe11de6c484211
Luca Kaiser <Luca.Kaiser01@sap.com>
2022-07-05 09:58:31 +0200

    Add Game Class and it's responding Tests

--- src/test/java/hwr/oop/BowlingTest.java
+++ src/test/java/hwr/oop/BowlingTest.java
@@ -1,8 +1,82 @@
 package hwr.oop;
 
-public class BowlingTest {
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-    //TODO
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BowlingTest {
+    Game game = new Game();
+
+    @Nested
+    class IsInstanceOfTest {
+        @Test
+        void testInstances() {
+            assertThat(game).isInstanceOf(Game.class);
+        }
+    }
+
+    @Nested
+    class GameTests {
+        @Test
+        void getWorstGame() {
+            rollMany(20,0);
+            assertThat(game.score()).isZero();
+        }
+
+        @Test
+        void isValidGame() {
+            rollMany(20,1);
+            //ToDo
+        }
+
+        @Test
+        void isInvalidGame() {
+            //ToDo
+        }
+
+        @Test
+        void getPerfectGame() {
+            rollMany(12,10);
+            assertThat(game.score()).isEqualTo(300);
+        }
+
+        @Test
+        void isStrike() {
+            rollStrike();
+            game.roll(4);
+            game.roll(3);
+            rollMany(16,0);
+            assertThat(game.score()).isEqualTo(24);
+        }
+
+        @Test
+        void isSpare() {
+            rollSpare();
+            game.roll(4);
+            rollMany(17,0);
+            assertThat(game.score()).isEqualTo(18);
+        }
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollSpare() {
+        game.roll(4);
+        game.roll(6);
+    }
+}
+
+
+//TODO
     /*
 Spare, Strike = 10P
 
@@ -36,5 +110,3 @@ Spare, Strike = 10P
 - Game
 - ScoreFrame
 */
-
-}
